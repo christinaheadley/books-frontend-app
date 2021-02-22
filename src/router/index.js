f7d8f12ee3fa15e52e920fa55ebe559b53ad2e5f
christina <chrstnaheadley@gmail.com>
2021-02-22 11:37:56 -0500

    adds authentication

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import axios from "axios";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
@@ -14,11 +17,9 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  // {
-  //   path: "/books/new",
-  //   name: "books-new",
-  //   component: BooksNew,
-  // },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
 ];
 
 const router = new VueRouter({
